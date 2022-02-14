2bd53bbd5429b8fa38145656a68f01203d44ea42
mark953fps <markville953@gmail.com>
2022-02-15 00:47:40 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,46 @@ import { Component, VERSION, OnInit, HostListener } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  tabIndex: number = 1;
 
   @HostListener('document: keydown', ['$event']) onKeyDown(e: any) {
     console.log(e, 'haha');
     let key = e.key;
 
     if (key === 'Tab') {
+      this.tabIndex++;
       e.preventDefault();
-      document.getElementById('tabindex4').focus();
+
+      switch (this.tabIndex) {
+        case 1:
+          document.getElementById('tabindex1').focus();
+          break;
+
+        case 2:
+          document.getElementById('tabindex2').focus();
+          break;
+
+        case 3:
+          document.getElementById('tabindex3').focus();
+          break;
+
+        case 4:
+          document.getElementById('tabindex4').focus();
+          break;
+
+        case 5:
+          document.getElementById('tabindex5').focus();
+          break;
+
+        default:
+          this.tabIndex = 1;
+      }
     }
   }
 
   public ngOnInit() {}
 
-  test() {
-    console.log('hello')
+  focusIndex(tabIndex: any) {
+    this.tabIndex = tabIndex;
   }
 }
