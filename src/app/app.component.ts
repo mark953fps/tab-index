f95cfccd9d48c4f0f50274eac3e74cc39add504a
mark953fps <markville953@gmail.com>
2022-02-15 00:41:47 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  @HostListener('document: keydown', ['$event']) onKeyDown(e: any) {
+    console.log(e, 'haha');
+    let key = e.key;
+
+    if (key === 'Tab') {
+      e.preventDefault();
+      document.getElementById('tabindex4').focus();
+    }
+  }
+
+  public ngOnInit() {}
+
+  test() {
+    console.log('hello')
+  }
 }
